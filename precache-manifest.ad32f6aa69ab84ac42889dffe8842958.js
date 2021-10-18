8913c02623af43855fb6321235a6dc8bc7db2277
Aleksandra <astarta1107@gmail.com>
2021-10-18 16:25:55 +0300

    deploy

---
+++ precache-manifest.ad32f6aa69ab84ac42889dffe8842958.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9dd1f1a0b3685054db",
+    "url": "/vue-task-app/css/app.90a7ffb1.css"
+  },
+  {
+    "revision": "75e2fe7c3304f5f5f7fe",
+    "url": "/vue-task-app/css/chunk-2bb937c5.5694190f.css"
+  },
+  {
+    "revision": "bff38ad011bb2a7b181a7e4369514b92",
+    "url": "/vue-task-app/index.html"
+  },
+  {
+    "revision": "5e9dd1f1a0b3685054db",
+    "url": "/vue-task-app/js/app.3145d415.js"
+  },
+  {
+    "revision": "75e2fe7c3304f5f5f7fe",
+    "url": "/vue-task-app/js/chunk-2bb937c5.5e66e826.js"
+  },
+  {
+    "revision": "44c363fb1bebfa6344f9",
+    "url": "/vue-task-app/js/chunk-2d0b64c8.fb87a865.js"
+  },
+  {
+    "revision": "9df0316d34532d400e95",
+    "url": "/vue-task-app/js/chunk-vendors.1f25baff.js"
+  },
+  {
+    "revision": "2955b4160cb6993bf1d84fe842592efa",
+    "url": "/vue-task-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-task-app/robots.txt"
+  }
+]);
